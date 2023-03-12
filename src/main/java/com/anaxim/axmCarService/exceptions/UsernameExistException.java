457a6a9b64ce2430ccdbd2f9f0fa8fcc449e9acd
damdravic <damian.dragosv@gmail.com>
2023-03-12 22:48:58 +0200

    some features added

---
+++ src/main/java/com/anaxim/axmCarService/exceptions/UsernameExistException.java
@@ -0,0 +1,8 @@
+package com.anaxim.axmCarService.exceptions;
+
+public class UsernameExistException extends Exception {
+    public UsernameExistException(String msg) {
+
+        super(msg);
+    }
+}
